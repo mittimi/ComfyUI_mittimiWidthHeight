4fb598e5fe6f428d5853bcf866d106cb0a476ea6
mittimi <mittings@gmail.com>
2025-08-27 16:49:21 +0900

    Update web.js

--- js/web.js
+++ js/web.js
@@ -1,11 +1,42 @@
 import { ComfyApp, app } from "../../scripts/app.js";
 
 
+var allow_set_flag = true;
+
+
 app.registerExtension({
 	name: "ComfyUI_mittimiWidthHeight",
 
-    async nodeCreated(node) {
+
+    async beforeConfigureGraph() {
+        allow_set_flag = false;
+    },
+
+
+    async nodeCreated(node) {        
+
         if (node.comfyClass == "WidthHeightMittimi01") {
+
+            Object.defineProperty(node.widgets[2], "value", {
+
+                set: (value) => {
+
+                    node._value = value;
+
+                    
+                    if (allow_set_flag) {
+                        var splitvalue = value.split("x");
+                        node.widgets[0].value = parseInt( splitvalue[0] );
+                        node.widgets[1].value = parseInt( splitvalue[1] );
+                    }
+                    
+                },
+
+                get: () => {
+                    return node._value;
+                }
+                
+            });
 
             node.addWidget("button", "Swap", "value maybe?", (...args) => {
                 
@@ -14,9 +45,10 @@ app.registerExtension({
                 node.widgets[1].value = wv;
             })
         }
+    }, 
+
+
+    async afterConfigureGraph() {
+        allow_set_flag = true;
     }
 });
-
-
-
-    
